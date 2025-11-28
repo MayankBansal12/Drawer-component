dbf089c3989f96ad3eb1e40302dfd668f11725cc
Mayank Bansal <mayankbansal125@gmail.com>
2025-11-28 11:58:49 +0530

    intial react, shadcn setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { DrawerMenu } from '@/components/drawer-menu'
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <DrawerMenu />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/drawer-menu.tsx
@@ -0,0 +1,33 @@
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
+import { Button } from "@/components/ui/button"
+
+const DrawerMenu = () => {
+    return (
+        <Drawer>
+            <DrawerTrigger className="border py-2 px-4 rounded-full text-white bg-blue-500 cursor-pointer">Open Menu</DrawerTrigger>
+            <DrawerContent className="m-4">
+                <DrawerHeader>
+                    <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                    <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                </DrawerHeader>
+                <DrawerFooter>
+                    <Button asChild>Submit</Button>
+                    <DrawerClose>
+                        <Button variant="outline">Cancel</Button>
+                    </DrawerClose>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+    )
+}
+
+export { DrawerMenu }
